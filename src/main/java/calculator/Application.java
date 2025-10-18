84f9539531ec131efd59cd65fe8e64cda18cbdc2
leechungheon <leechung6311@gmail.com>
2025-10-18 19:04:56 +0900

    fix(app): NoSuchElementException 발생 시 프로그램 종료 오류 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,24 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-
-        boolean isCustomDelimiter = Validator.isCustomDelimiter(input);
-        if (isCustomDelimiter) {
-            int result = Calculator.add(input, Validator.extractDelimiter(input));
-            System.out.println("결과 : " + result);
-        } else {
-            int result = Calculator.add(input, null);
-            System.out.println("결과 : " + result);
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine(); // 예외 발생 가능
+            boolean isCustomDelimiter = Validator.isCustomDelimiter(input);
+            if (isCustomDelimiter) {
+                int result = Calculator.add(input, Validator.extractDelimiter(input));
+                System.out.println("결과 : " + result);
+            } else {
+                int result = Calculator.add(input, null);
+                System.out.println("결과 : " + result);
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("결과 : " + 0);
         }
     }
 }
