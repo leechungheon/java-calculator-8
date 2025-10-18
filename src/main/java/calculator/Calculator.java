3db45f63b90d771450096cdbe49b49f244eef458
leechungheon <leechung6311@gmail.com>
2025-10-18 17:56:50 +0900

    refactor(Calculator): isNumber 메서드를 분리하여 숫자 검증 명확화

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -18,12 +18,21 @@ public class Calculator {
                 return 0;
             } else if (numberString.contains("-")) { // 2. 음수 입력 시 예외 처리
                 throw new IllegalArgumentException("음수는 허용되지 않습니다.");
-            } else if (texts.length == 1) { // 3. 한 자리 숫자라면 해당 숫자를 반환
-                return Integer.parseInt(numberString);
-            } else { // 4. 그 외의 경우에는 기본 구분자로 구분된 모든 숫자의 합을 반환
+            } else if (isNumber(numberString)) { // 4. 그 외의 경우에는 기본 구분자로 구분된 모든 숫자의 합을 반환
                 sum += Integer.parseInt(numberString);
+            } else {
+                throw new IllegalArgumentException("숫자가 아닌 문자는 허용되지 않습니다.");
             }
         }
         return sum;
+    }
+
+    public static boolean isNumber(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
