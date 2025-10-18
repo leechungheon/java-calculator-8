84d0db989c0c9faadc50ed688f3c150fc09b6308
leechungheon <leechung6311@gmail.com>
2025-10-18 19:59:13 +0900

    fix(Input): 입력 출력문 양식 누락 수정

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     public static String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 }
