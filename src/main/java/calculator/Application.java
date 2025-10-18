7b9b5237d4cb9a9d475ed7718b3739b9d558d49a
leechungheon <leechung6311@gmail.com>
2025-10-18 19:54:00 +0900

    refactor(app): 입출력 클래스 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,24 +1,22 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int result;
         try {
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String input = Console.readLine(); // 예외 발생 가능
+            String input = Input.input();
             boolean isCustomDelimiter = Validator.isCustomDelimiter(input);
             if (isCustomDelimiter) {
-                int result = Calculator.add(input, Validator.extractDelimiter(input));
-                System.out.println("결과 : " + result);
+                result = Calculator.add(input, Validator.extractDelimiter(input));
             } else {
-                int result = Calculator.add(input, null);
-                System.out.println("결과 : " + result);
+                result = Calculator.add(input, null);
             }
+            Output.output(result);
         } catch (NoSuchElementException e) {
-            System.out.println("결과 : " + 0);
+            Output.output(0);
         }
     }
 }
