8d5f6e7642987a35a0fe00e1ed8b0cd40fe22c17
leechungheon <leechung6311@gmail.com>
2025-10-19 20:18:52 +0900

    refactor(Checker): 오버플로우 예외 메시지 분리

--- src/main/java/calculator/Checker.java
+++ src/main/java/calculator/Checker.java
@@ -1,14 +1,24 @@
 package calculator;
 
 public class Checker {
+    private static boolean isAllDigits(String numberString) {
+        return numberString.matches("^\\d+$");
+    }
+
     public static void positiveNumber(String numberString) {
+        // 1. 음수 검사
         if (numberString.contains("-")) {
             throw new IllegalArgumentException("음수는 허용되지 않습니다.");
         }
+        // 2. 숫자가 아닌 문자 포함 여부 검사
+        if (!isAllDigits(numberString)) {
+            throw new IllegalArgumentException("숫자가 아닌 문자는 허용되지 않습니다.");
+        }
+        // 3. 오버플로우 검사
         try {
             Integer.parseInt(numberString);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 문자는 허용되지 않습니다.");
+            throw new IllegalArgumentException("허용되는 정수 범위를 초과합니다.");
         }
     }
 }
